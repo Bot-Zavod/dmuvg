e22b0aa2d742982239485750d9a4e9c5d8f72345
Alexey <almarka03@gmail.com>
2020-06-12 22:16:15 +0300

    ...

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+	return render(request, "index.html")
+
+def article_edit(request):
+	return render(request, "article_edit.html")
+
+def article_feed(request):
+	return render(request, "article_feed.html")
+
+def article(request):
+	return render(request, "article.html")
